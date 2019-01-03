76582ee14013d0a1b406818f8e0f9a31e4815159
Szczepan Masny <szczepanmasny@gmail.com>
2019-01-03 13:06:28 +0100

    fix

--- templates/locales/en/index.js
+++ templates/locales/en/index.js
@@ -1,14 +1,16 @@
 import alerts from './alerts.js'
 import routes from './routes.js'
 import login from './login.js'
+import datatable from './datatable.js'
 
 export default {
     global: {
-      alerts: alerts,
-      routes: routes,
-      login: login
+      alerts,
+      routes,
+      login,
+      datatable
     },
     buttons: {
-      goToItem: 'Details',
+      goToItem: "Szczegóły",
     },
 }
