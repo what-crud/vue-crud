d36582e0313a80173d51720a5b6ec8bbbbf58e67
Szczepan Masny <szczepanmasny@gmail.com>
2019-02-21 22:31:40 +0100

    cleanup in main router

---
+++ examples/crm/router.js
@@ -0,0 +1,61 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from './routes/app/Index'
+import appRoutes from './routes/app/router.js'
+import Login from './routes/login/Index'
+import store from '@/store/'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [{
+      path: '/',
+      name: 'app',
+      component: App,
+      children: appRoutes,
+      beforeEnter: (to, from, next) => {
+        var auth = localStorage.getItem('token')
+        if (!auth){
+            store.commit('auth/logout')
+            next('/login')
+        }
+        else {
+          Vue.http.get('auth/user')
+            .then(response => {
+              next()
+            }, response => {
+              next('/login')
+            });
+        }
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+  ]
+})
+
+router.beforeEach(function (to, from, next) {
+  let middleware
+  store.state.page = null
+  to.matched.some(m => middleware = m.meta.guard)
+  if(typeof middleware === "undefined") {
+    next()
+  } 
+  else{
+    if(store.getters['auth/checkPermission'](middleware)){
+      window.scrollTo(0, 0);
+      next()
+    }
+    else if (store.getters['auth/isLogged']) {
+      next('/home')
+    }
+    else{
+      next('/login')
+    }
+  }
+});
+
+export default router
--- src/router.js
+++ src/router.js
@@ -1,62 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Main from './routes/app/Index'
-import mainRoutes from './routes/app/router.js'
-import Login from './routes/login/Index'
-import store from '@/store/'
 
 Vue.use(Router)
 
 const router = new Router({
-  routes: [{
-      path: '/',
-      name: 'main',
-      component: Main,
-      children: mainRoutes,
-      beforeEnter: (to, from, next) => {
-        var auth = localStorage.getItem('token')
-        if (!auth){
-            store.commit('auth/logout')
-            next('/login')
-        }
-        else {
-          Vue.http.get('auth/user')
-            .then(response => {
-              next()
-            }, response => {
-              next('/login')
-            });
-          //store.dispatch('auth/getUser').then(next())
-        }
-      }
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-  ]
+  routes: []
 })
-
-router.beforeEach(function (to, from, next) {
-  let middleware
-  store.state.page = null
-  to.matched.some(m => middleware = m.meta.guard)
-  if(typeof middleware === "undefined") {
-    next()
-  } 
-  else{
-    if(store.getters['auth/checkPermission'](middleware)){
-      window.scrollTo(0, 0);
-      next()
-    }
-    else if (store.getters['auth/isLogged']) {
-      next('/home')
-    }
-    else{
-      next('/login')
-    }
-  }
-});
 
 export default router
