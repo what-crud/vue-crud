c6d257cf0f7a035a246f9cf396757553ad482b6c
Szczepan Masny <szczepanmasny@gmail.com>
2019-02-21 23:49:06 +0100

    added simple-crud example

---
+++ examples/simple-crud/locales/pl/index.js
@@ -0,0 +1,13 @@
+import alerts from './alerts.js'
+import datatable from './datatable.js'
+import details from './details.js'
+import itemElements from './item-elements.js'
+
+export default {
+    global: {
+      alerts,
+      datatable,
+      details,
+      itemElements,
+    },
+}
