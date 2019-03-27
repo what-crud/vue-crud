d86d07270d6382bd699169e0b0ddac29251ca114
Szczepan Masny <szczepanmasny@gmail.com>
2019-03-27 22:21:22 +0100

    #23 - Added template wizard and empty template

---
+++ examples/empty/locales/en/index.js
@@ -0,0 +1,13 @@
+import alerts from './alerts'
+import datatable from './datatable'
+import details from './details'
+import itemElements from './item-elements'
+
+export default {
+  global: {
+    alerts,
+    datatable,
+    details,
+    itemElements
+  }
+}
