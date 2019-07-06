f30b0ac9789814b3a6e2c1646be30dfd1c0f998e
Szczepan Masny <szczepanmasny@gmail.com>
2019-07-07 00:30:34 +0200

    update login locales

--- examples/sandbox/locales/pl/login.js
+++ examples/sandbox/locales/pl/login.js
@@ -1,10 +1,12 @@
 export default {
-  title: 'Vue CRUD',
-  email: 'E-mail',
+  title: 'Your App',
+  login: 'E-mail',
+  username: 'Nazwa użytkownika',
   password: 'Hasło',
   failed: 'Próba zalogowania nie powiodła się',
   submit: 'Zaloguj',
   passwordRequired: 'Hasło jest wymagane',
-  emailRequired: 'Adres e-mail jest wymagany',
-  incorrectEmail: 'E-mail jest niepoprawny'
+  incorrectPassword: 'Hasło ma niepoprawny format',
+  loginRequired: 'Adres e-mail jest wymagany',
+  incorrectLogin: 'E-mail ma niepoprawny format'
 }
