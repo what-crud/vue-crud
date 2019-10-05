c548c80230b760faa2d152893ac93d0035c989ba
Szczepan Masny <szczepanmasny@gmail.com>
2019-10-05 16:51:51 +0200

    fix tooltips in DataTableControls

--- src/utils/crud/components/DataTableControls.vue
+++ src/utils/crud/components/DataTableControls.vue
@@ -5,38 +5,76 @@
       <v-flex xs12 lg3 text-xs-center>
         <!-- Dialog for creating item -->
         <v-tooltip top v-if="createMode">
-          <v-btn fab dark color="light-blue lighten-2" @click="create()" slot="activator">
-            <v-icon>add</v-icon>
-          </v-btn>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              fab dark
+              color="light-blue lighten-2"
+              @click="create()"
+              v-on="on"
+            >
+              <v-icon>add</v-icon>
+            </v-btn>
+          </template>
           <span>{{ $t('global.datatable.add') }}</span>
         </v-tooltip>
         <!-- Multiple edit -->
         <v-tooltip top v-if="editMode && selectManyMode && updateManyMode">
-          <v-btn fab small dark color="orange" @click="editSelected()" slot="activator">
-            <v-icon>edit</v-icon>
-          </v-btn>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              fab dark small
+              color="orange"
+              @click="editSelected()"
+              v-on="on"
+            >
+              <v-icon>edit</v-icon>
+            </v-btn>
+          </template>
           <span>{{ $t('global.datatable.buttons.editSelected') }}</span>
         </v-tooltip>
         <!-- suspend/restore record (if soft deletes are enabled) -->
         <template v-if="['soft', 'both'].includes(deleteMode) && selectManyMode && updateManyMode">
           <v-tooltip top>
-            <v-btn class="white--text" fab small color="red" @click="suspendSelected()" slot="activator">
-              <v-icon>undo</v-icon>
-            </v-btn>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                fab dark small
+                color="red"
+                @click="suspendSelected()"
+                v-on="on"
+              >
+                <v-icon>undo</v-icon>
+              </v-btn>
+            </template>
             <span>{{ $t('global.datatable.buttons.suspendSelected') }}</span>
           </v-tooltip>
           <v-tooltip top>
-            <v-btn class="white--text" fab small color="green" @click="restoreSelected()" slot="activator">
-              <v-icon>redo</v-icon>
-            </v-btn>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                fab dark small
+                color="green"
+                @click="restoreSelected()"
+                v-on="on"
+              >
+                <v-icon>redo</v-icon>
+              </v-btn>
+            </template>
             <span>{{ $t('global.datatable.buttons.restoreSelected') }}</span>
           </v-tooltip>
         </template>
         <!-- hard delete -->
-        <v-tooltip top v-if="['hard', 'both'].includes(deleteMode) && selectManyMode && removeManyMode">
-          <v-btn class="white--text" fab small color="black" @click="destroySelected()" slot="activator">
-            <v-icon>delete</v-icon>
-          </v-btn>
+        <v-tooltip
+          top
+          v-if="['hard', 'both'].includes(deleteMode) && selectManyMode && removeManyMode"
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+              fab dark small
+              color="black"
+              @click="destroySelected()"
+              v-on="on"
+            >
+              <v-icon>delete</v-icon>
+            </v-btn>
+          </template>
           <span>{{ $t('global.datatable.buttons.deleteSelected') }}</span>
         </v-tooltip>
         <slot name="left"></slot>
@@ -45,16 +83,16 @@
       <v-flex xs12 lg6 text-xs-center>
         <slot name="center"></slot>
         <v-tooltip top v-if="mainFilter || fieldFilters">
-          <v-btn
-            class="white--text"
-            fab
-            small
-            color="red"
-            @click="clearFilters()"
-            slot="activator"
-          >
-            <v-icon>delete_sweep</v-icon>
-          </v-btn>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              fab dark small
+              color="red"
+              @click="clearFilters()"
+              v-on="on"
+            >
+              <v-icon>delete_sweep</v-icon>
+            </v-btn>
+          </template>
           <span>{{ $t('global.datatable.buttons.clearFilters') }}</span>
         </v-tooltip>
       </v-flex>
@@ -62,16 +100,16 @@
       <v-flex xs12 lg3 text-xs-center>
         <!-- Refresh table -->
         <v-tooltip top v-if="refreshButton">
-          <v-btn
-            class="white--text"
-            fab
-            small
-            color="blue"
-            @click="refreshTable()"
-            slot="activator"
-          >
-            <v-icon>refresh</v-icon>
-          </v-btn>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              fab dark small
+              color="blue"
+              @click="refreshTable()"
+              v-on="on"
+            >
+              <v-icon>refresh</v-icon>
+            </v-btn>
+          </template>
           <span>{{ $t('global.datatable.buttons.refreshTable') }}</span>
         </v-tooltip>
         <!-- Clear filters -->
