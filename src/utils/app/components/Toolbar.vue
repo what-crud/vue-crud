e34e330155f18f769cf672883849bc1d6153f74d
Szczepan Masny <szczepanmasny@gmail.com>
2019-10-05 16:22:05 +0200

    add BEM to Toolbar

--- src/utils/app/components/Toolbar.vue
+++ src/utils/app/components/Toolbar.vue
@@ -1,22 +1,31 @@
 <template>
-  <v-app-bar :dark="dark" :color="color" fixed app>
-
+  <v-app-bar
+    class="toolbar"
+    :dark="dark"
+    :color="color"
+    fixed
+    app
+  >
     <slot name="left"></slot>
     <v-spacer></v-spacer>
     <router-link :to="titleLink">
-      <img v-if="showLogo" class="logo" :src="require(`@/assets/images/${logo}`)">
+      <img
+        v-if="showLogo"
+        class="toolbar__logo"
+        :src="require(`@/assets/images/${logo}`)"
+      >
     </router-link>
     <router-link :to="titleLink">
-      <v-toolbar-title class="title">
+      <v-toolbar-title class="toolbar__title">
         {{title}}
       </v-toolbar-title>
     </router-link>
     <v-spacer></v-spacer>
     <slot name="right"></slot>
     <v-menu
+      v-if="localesBtn"
       menu-props="bottom"
       left
-      v-if="localesBtn"
     >
       <template v-slot:activator="{ on }">
         <v-btn
@@ -27,15 +36,29 @@
         </v-btn>
       </template>
       <v-list>
-        <v-list-item v-for="(locale, i) in locales" :key="i" @click="changeLocale(locale.name)">
-          <v-list-item-title>{{ locale.text }}</v-list-item-title>
+        <v-list-item
+          v-for="(locale, i) in locales"
+          :key="i"
+          @click="changeLocale(locale.name)"
+        >
+          <v-list-item-title>
+            {{ locale.text }}
+          </v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
-    <v-btn v-if="profileBtn" icon @click.stop="openProfileDialog">
+    <v-btn
+      v-if="profileBtn"
+      icon
+      @click.stop="openProfileDialog"
+    >
       <v-icon>perm_identity</v-icon>
     </v-btn>
-    <a v-if="logoutBtn" v-on:click="logoutAttempt()">
+    <a
+      v-if="logoutBtn"
+      class="toolbar__logout-button"
+      v-on:click="logoutAttempt()"
+    >
       <v-btn icon>
         <v-icon>power_settings_new</v-icon>
       </v-btn>
@@ -122,24 +145,22 @@ export default {
 }
 
 </script>
-<style scoped>
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-  .logo {
+<style lang="scss" scoped>
+.toolbar {
+  &__logo {
     height:50px;
     width:auto;
-    margin: 0 10px 0 10px;
-  }
-  .title {
-    margin: 0 10px 0 10px;
-  }
-  @media (max-width: 699px) {
-    .logo {
-
+    margin: 0 10px;
+    @media (max-width: 699px) {
       display:none;
     }
   }
-
+  &__title {
+    margin: 0 10px 0 10px;
+  }
+  &__logout-button {
+    color: inherit;
+    text-decoration: none;
+  }
+}
 </style>
