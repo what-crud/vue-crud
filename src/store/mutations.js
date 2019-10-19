e2d7b083c46d3f1e89e6904f17e01702fbf3f5de
Szczepan Masny <szczepanmasny@gmail.com>
2019-10-19 17:09:09 +0200

    fix displaying alerts

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -7,7 +7,7 @@ const mutations = {
   alertSuccess (state, text) {
     state.alert.type = 'success'
     state.alert.text = text
-    state.alert.show = 1
+    state.alert.show = true
   },
   alertValidationError (state, obj) {
     state.alert.type = 'warning'
@@ -16,15 +16,15 @@ const mutations = {
       text += `<div><strong>${key}: </strong>${obj[key].join(', ')}</div>`
     }
     state.alert.text = text
-    state.alert.show = 1
+    state.alert.show = true
   },
   alertError (state, text) {
     state.alert.type = 'error'
     state.alert.text = text
-    state.alert.show = 1
+    state.alert.show = true
   },
   closeAlertBox (state) {
-    state.alert.show = 0
+    state.alert.show = false
   }
 }
 
