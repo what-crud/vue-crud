526cc2adc0183baab56be318937aee00abad84ac
Szczepan Masny <szczepanmasny@gmail.com>
2019-11-05 22:57:01 +0100

    #50 add tooltips to buttons

--- src/utils/crud/components/field-types/RichTextBox.vue
+++ src/utils/crud/components/field-types/RichTextBox.vue
@@ -8,94 +8,94 @@
         <menu-button
           icon="format_bold"
           :active="isActive.bold()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.bold')"
           @clicked="commands.bold"
         />
         <menu-button
           icon="format_italic"
           :active="isActive.italic()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.italic')"
           @clicked="commands.italic"
         />
         <menu-button
           icon="format_strikethrough"
           :active="isActive.strike()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.strike')"
           @clicked="commands.strike"
         />
         <menu-button
           icon="format_underlined"
           :active="isActive.underline()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.underline')"
           @clicked="commands.underline"
         />
         <menu-button
           icon="code"
           :active="isActive.code()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.code')"
           @clicked="commands.code"
         />
         <menu-button
           text="P"
           :active="isActive.paragraph()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.paragraph')"
           @clicked="commands.paragraph"
         />
         <menu-button
           text="H1"
           :active="isActive.heading({ level: 1 })"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.heading1')"
           @clicked="commands.heading({ level: 1 })"
         />
         <menu-button
           text="H2"
           :active="isActive.heading({ level: 2 })"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.heading2')"
           @clicked="commands.heading({ level: 2 })"
         />
         <menu-button
           text="H3"
           :active="isActive.heading({ level: 3 })"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.heading3')"
           @clicked="commands.heading({ level: 3 })"
         />
         <menu-button
           icon="format_list_bulleted"
           :active="isActive.bullet_list()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.bulletedList')"
           @clicked="commands.bullet_list"
         />
         <menu-button
           icon="format_list_numbered"
           :active="isActive.ordered_list()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.numberedList')"
           @clicked="commands.ordered_list"
         />
         <menu-button
           icon="format_quote"
           :active="isActive.blockquote()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.blockquote')"
           @clicked="commands.blockquote"
         />
         <menu-button
           icon="code"
           :active="isActive.code_block()"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.codeBlock')"
           @clicked="commands.code_block"
         />
         <menu-button
           icon="minimize"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.horizontalRule')"
           @clicked="commands.horizontal_rule"
         />
         <menu-button
           icon="undo"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.undo')"
           @clicked="commands.undo"
         />
         <menu-button
           icon="redo"
-          :tooltip="$t('richTextBox.')"
+          :tooltip="$t('global.richTextBox.redo')"
           @clicked="commands.redo"
         />
       </div>
